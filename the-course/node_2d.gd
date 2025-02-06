4adcbaf90b993eed9f0bd018478764a874fc18fd
D24125405 Enkhtushig Legdenbold <D24125405@mytudublin.ie>
2025-02-06 12:51:04 +0000

    added color and radius

--- the-course/node_2d.gd
+++ the-course/node_2d.gd
@@ -1,12 +1,32 @@
 extends Node2D
 
+var radius:float
+var count:int
+
 func _draw() -> void:
 	var l = $num_lines.value
 	for i in range(l):
 		draw_line(Vector2(i * 100,0), Vector2(i * 100, 500), Color.BEIGE, 10)
+	
+	
+	var theta_inc = PI * 2.0 / l
+	for i in range(l):
+		var theta = theta_inc * i
+		var x = sin(theta) * radius
+		var y = cos(theta) * radius
+		draw_circle(Vector2(x, y), radius, Color.CORNSILK, false)
 		
 		
 		pass
 		
 func _process(delta: float) -> void:
 	queue_redraw()
+	
+func _on_num_lines_value_changed(value: float) -> void:
+	count = value
+	pass 
+	
+func _on_radius_value_changed(value: float) -> void:
+	radius = value
+	pass
+	
